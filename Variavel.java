2aa1ade0638aaed8c4bd34c611d9cdfc5d4fa6a9
GyovanneMS <98404327+GyovanneMS@users.noreply.github.com>
2022-03-13 21:21:08 -0300

    Update Variavel.java

--- Variavel.java
+++ Variavel.java
@@ -10,12 +10,12 @@ class Variavel {
         System.out.println("Segundo número: ");
         int N2 = ler.nextInt();
 
-        System.out.print("O primeiro número é: " + N1 + ", e o sgndo é: " + N2);
+        System.out.print("O primeiro número é: " + N1 + ", e o segundo é: " + N2);
 
         int N3 = N1;
         N1 = N2;
         N2 = N3;
 
-        System.out.print("Agora o primeiro número é: " + N1 + ", e o sgndo é: " + N2);        
+        System.out.print("Agora o primeiro número é: " + N1 + ", e o segundo é: " + N2);        
     }
 }
